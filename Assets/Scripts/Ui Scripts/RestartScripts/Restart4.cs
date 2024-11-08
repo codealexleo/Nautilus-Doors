7305ec2b12e2c9ef11b275dee96169c5d483b684
codealexleo <alexandremconstancio@gmail.com>
2024-11-08 15:47:04 +0000

    Doors fixed and added restart and next level scenes

---
+++ Assets/Scripts/Ui Scripts/RestartScripts/Restart4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart4 : MonoBehaviour
+{
+    private void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void RestartScene()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+
+}
